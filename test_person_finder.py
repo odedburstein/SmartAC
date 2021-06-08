9391c900fd0a1cd461963f635896faff0a28e9fc
dalevin95 <49447299+dalevin95@users.noreply.github.com>
2021-06-08 15:44:50 +0300

    Update test_person_finder.py

--- test_person_finder.py
+++ test_person_finder.py
@@ -6,69 +6,76 @@
 # You can follow this installation instructions to get your RPi set up:
 # https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
 
-import pyrealsense2.pyrealsense2 as rs
-import numpy as np
-import cv2
 import face_recognition
 import picamera
 import numpy as np
-
-from multiprocessing import Queue
-from queue import Empty
+import pyrealsense2.pyrealsense2 as rs
+import cv2
 
 # Get a reference to the Raspberry Pi camera.
 # If this fails, make sure you have a camera connected to the RPi and that you
 # enabled your camera in raspi-config and rebooted first.
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 6)
-config.enable_stream(rs.stream.color, 640, 360, rs.format.bgr8, 30)
-print("[INFO] Starting streaming...")
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 6)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+print(f"{__file__} Starting streaming...")
 pipeline.start(config)
-print("[INFO] Camera ready.")
+print(f"{__file__} Camera ready.")
 
 # Load a sample picture and learn how to recognize it.
 print("Loading known face image(s)")
-oded_image = face_recognition.load_image_file("oded_small.jpg")
-oded_face_encoding = face_recognition.face_encodings(oded_image)[0]
+photo_path = "naveh_small.jpeg"
+name = photo_path.split('_')[0]
+obama_image = face_recognition.load_image_file(photo_path)
+user_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+align_to = rs.stream.color
+align = rs.align(align_to)
 
 # Initialize some variables
 face_locations = []
 face_encodings = []
-face_names = ["oded"]
-should_work = False
-msg = None
-print("Person finder finished calibrating")
-
 if __name__=="__main__":
     while True:
         print("Capturing image.")
         # Grab a single frame of video from the RPi camera as a numpy array
         frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
 
-        color_image = np.asanyarray(color_frame.get_data())
+        aligned_frames = align.process(frames)
+        aligned_depth_frame = aligned_frames.get_depth_frame()
+        color_frame = aligned_frames.get_color_frame()
+        output = np.asanyarray(color_frame.get_data())
 
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(color_image)
+        face_locations = face_recognition.face_locations(output)
         print("Found {} faces in image.".format(len(face_locations)))
-        face_encodings = face_recognition.face_encodings(color_image, face_locations)
+        face_encodings = face_recognition.face_encodings(output, face_locations)
 
+        location = None
         # Loop over each face found in the frame to see if it's someone we know.
-        for face_encoding in face_encodings:
+        for i, face_encoding in enumerate(face_encodings):
             # See if the face is a match for the known face(s)
-            match = face_recognition.compare_faces([oded_face_encoding], face_encoding)
-            name = "<Unknown Person>"
-
+            match = face_recognition.compare_faces([user_face_encoding], face_encoding)
             if match[0]:
-                name = "Oded"
+                location = face_locations[i]
+                print(f"I found {name}")
 
-            print("I see someone named {}!".format(name))
+        if location != None:
+            # Display the results
+            for (top, right, bottom, left), name in zip([location], [name]):
 
-        for (top, right, bottom, left) in face_locations:
+                # Draw a box around the face
+                cv2.rectangle(output, (left, top), (right, bottom), (0, 0, 255), 2)
 
-            # Draw a box around the face
-            cv2.rectangle(color_image, (left, top), (right, bottom), (0, 0, 255), 2)
+                # Draw a label with a name below the face
+                cv2.rectangle(output, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+                font = cv2.FONT_HERSHEY_DUPLEX
+                cv2.putText(output, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-            # Draw a label with a name below the face
-            cv2.rectangle(color_image, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            # Display the resulting image
+
+        cv2.imshow('Video', output)
+        cv2.waitKey(5)
+        

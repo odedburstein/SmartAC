1a6e572047dbd211ff7711f470787f57537f4d77
dalevin95 <49447299+dalevin95@users.noreply.github.com>
2021-06-09 12:45:46 +0300

    Update smart_ac_orchestrator.py

--- smart_ac_orchestrator.py
+++ smart_ac_orchestrator.py
@@ -1,8 +1,11 @@
 import bluetooth
 import multiprocessing
+import os
+import time
+import pyrebase
 
 from bluetooth import BluetoothSocket as socket
-from person_finder import person_finder
+from segmented_person_finder import person_finder
 
 # BT config
 server_sock = socket(bluetooth.RFCOMM)
@@ -14,7 +17,20 @@ server_sock.listen(1)
 queue = multiprocessing.Queue()
 
 person_finder_process = multiprocessing.Process(target=person_finder, args=(queue,))
-person_finder_process.start()
+
+#  Firebase config
+config = {
+  "apiKey": "AIzaSyD13Vy9h8KrFWDMG4-dZwcJhkxgJvk7smk",
+  "authDomain": "smart-ac-e68d3.firebaseapp.com",
+  "databaseURL": "https://smart-ac-e68d3-default-rtdb.europe-west1.firebasedatabase.app/",
+  "storageBucket": "smart-ac-e68d3.appspot.com"
+}
+
+path_to_user_photo = "user.jpg"
+user_photo_download_path = "user_small.jpg"
+
+firebase = pyrebase.initialize_app(config)
+
 
 def wait_for_connection():
     print(f"Smart AC orchestrator: Waiting for client to connect")
@@ -22,9 +38,31 @@ def wait_for_connection():
     print(f"Smart AC orchestrator: Accepted connection from {address}")
     return client_sock, address
 
+def get_user_photo_from_firebase():
+    try:
+        firebase_storage_client = firebase.storage()
+        firbase_storage_child = firebase_storage_client.child(path_to_user_photo)
+        firebase_download_response = firbase_storage_child.download(user_photo_download_path)
+        return firebase_download_response
+    except Exception as e:
+        print(f"Smart AC orchestrator: Failed to acess firebase. The error was {e}")
 
 if __name__ == '__main__':
+    print(f"Smart AC orchestrator: Starting orchestrator process")
+
+    print(f"Attempting to download user photo from firebase")
+    firebase_download_response = get_user_photo_from_firebase()
+
+    while not os.path.exists(user_photo_download_path):
+        time.sleep(1)
+
+    print(f"Sucessfully downloaded user photo from firebase")
+
+    print(f"Smart AC orchestrator: Starting person finder process")
+    person_finder_process.start()
+
     client_sock, address = wait_for_connection()
+
     while True:
         try:
             # Recieved messages from android are encoded

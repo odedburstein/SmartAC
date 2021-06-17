d8d6afd9da5ebdd18f76d83d906a8e7a0ed0afb8
dalevin95 <49447299+dalevin95@users.noreply.github.com>
2021-06-17 12:07:45 +0300

    Update smart_ac_orchestrator.py

--- smart_ac_orchestrator.py
+++ smart_ac_orchestrator.py
@@ -1,10 +1,12 @@
 import bluetooth
+import firebase_admin
 import multiprocessing
 import os
 import time
 import pyrebase
 
 from bluetooth import BluetoothSocket as socket
+#from firebase_admin import storage
 from segmented_person_finder import person_finder
 
 # BT config
@@ -18,19 +20,23 @@ queue = multiprocessing.Queue()
 
 person_finder_process = multiprocessing.Process(target=person_finder, args=(queue,))
 
-#  Firebase config
-config = {
-  "apiKey": "AIzaSyD13Vy9h8KrFWDMG4-dZwcJhkxgJvk7smk",
-  "authDomain": "smart-ac-e68d3.firebaseapp.com",
-  "databaseURL": "https://smart-ac-e68d3-default-rtdb.europe-west1.firebasedatabase.app/",
-  "storageBucket": "smart-ac-e68d3.appspot.com"
-}
-
 path_to_user_photo = "user.jpg"
 user_photo_download_path = "user_small.jpg"
 
-firebase = pyrebase.initialize_app(config)
+# firebase config
 
+firebase_config = {
+  'apiKey': "AIzaSyD13Vy9h8KrFWDMG4-dZwcJhkxgJvk7smk",
+  'authDomain': "smart-ac-e68d3.firebaseapp.com",
+  'projectId': "smart-ac-e68d3",
+  'storageBucket': "smart-ac-e68d3.appspot.com",
+  'messagingSenderId': "35189564918",
+  'appId': "1:35189564918:web:6b429e270002978844d3d7",
+    'databaseURL': 'https://smart-ac-e68d3-default-rtdb.europe-west1.firebasedatabase.app/'
+}
+
+firebase = pyrebase.initialize_app(firebase_config)
+storage = firebase.storage()
 
 def wait_for_connection():
     print(f"Smart AC orchestrator: Waiting for client to connect")
@@ -40,9 +46,8 @@ def wait_for_connection():
 
 def get_user_photo_from_firebase():
     try:
-        firebase_storage_client = firebase.storage()
-        firbase_storage_child = firebase_storage_client.child(path_to_user_photo)
-        firebase_download_response = firbase_storage_child.download(user_photo_download_path)
+        firebase_download_response = storage.child(path_to_user_photo).download(user_photo_download_path)
+
         return firebase_download_response
     except Exception as e:
         print(f"Smart AC orchestrator: Failed to acess firebase. The error was {e}")
@@ -50,16 +55,18 @@ def get_user_photo_from_firebase():
 if __name__ == '__main__':
     print(f"Smart AC orchestrator: Starting orchestrator process")
 
-    print(f"Attempting to download user photo from firebase")
+    print(f"Smart AC orchestrator: Attempting to download user photo from firebase")
     firebase_download_response = get_user_photo_from_firebase()
 
     while not os.path.exists(user_photo_download_path):
         time.sleep(1)
 
-    print(f"Sucessfully downloaded user photo from firebase")
+    person_finder_process.start()
+
+    print(f"Smart AC orchestrator: Sucessfully downloaded user photo from firebase")
 
     print(f"Smart AC orchestrator: Starting person finder process")
-    person_finder_process.start()
+
 
     client_sock, address = wait_for_connection()
 
@@ -73,8 +80,16 @@ if __name__ == '__main__':
             elif recieved_msg == 'OFF':
                 print(f"Smart AC orchestrator: Stopping work")
                 queue.put('OFF')
+            elif recieved_msg == "REFRESH_FACE":
+                firebase_download_response = get_user_photo_from_firebase()
+                print(f"Smart AC orchestrator: Changing user face")
+                queue.put('REFRESH_FACE')
+            elif recieved_msg == "REFRESH_POSITION":
+                print(f"Smart AC orchestrator: Changing ac position")
+                queue.put('REFRESH_POSITION')
             elif recieved_msg == 'EXIT':
-                queue.put('EXIT')
+                person_finder_process.kill()
+                queue.close()
                 print(f"Smart AC orchestrator: Going to sleep forever")
                 break
         except bluetooth.btcommon.BluetoothError as e:
